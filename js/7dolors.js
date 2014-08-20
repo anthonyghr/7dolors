a6d16decd31408b4a9a4fc116d1882666ebafe80
anthonyghr <agallagher@A-Gallaghers-iMac.local>
2014-08-19 21:34:53 -0400

    Tweaks, starting dolors page

---
+++ js/7dolors.js
@@ -0,0 +1,21 @@
+(function($) {
+    $(document).ready(function() {
+        // Change tab based on hash on page load
+        var url, tab;
+
+        url = $(location).attr('href');
+        if (url.match('#')) {
+            tab = url.split('#')[1];
+        } else {
+            tab = 'home';
+        }
+        $('.nav-tabs a[href=#' + tab + ']').tab('show');
+
+        // When clicking a tab, update the hash
+        $('.nav-tabs a').click(function(e) {
+            window.location.hash = e.target.hash;
+        });
+    });
+})(jQuery);
+
+
